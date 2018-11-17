c5e7ec62426bada2d5f4c83c5b5e7e2a0d0d5bb7
Joshua Ollendick <joshua.ollendick@gmail.com>
2018-11-17 10:15:51 -0600

    setup stock and stock panel

--- src/components/stocks/Stocks.vue
+++ src/components/stocks/Stocks.vue
@@ -1,4 +1,26 @@
 <template>
-    <h1>The Stocks Component</h1>
+    <div>
+       <app-stock v-for="stock in stocks" :key="stock.id"></app-stock> 
+    </div>
 </template>
+
+<script>
+    import Stock from './Stock.vue';
+
+    export default {
+        data() {
+            return {
+                stocks: [
+                    {id: 1, name: 'BMW', price: 110},
+                    {id: 2, name: 'Apple', price: 200},
+                    {id: 3, name: 'Google', price: 250},
+                    {id: 4, name: 'Twitter', price: 8}
+                ]
+            }
+        },
+        components: {
+            appStock: Stock
+        }
+    }
+</script>
 
