ed69b6e49304decec24ffabf8b0ecf9e6e3cb1a1
Joshua Ollendick <joshua.ollendick@gmail.com>
2018-11-17 10:42:40 -0600

    setup and modularize store

--- src/components/stocks/Stocks.vue
+++ src/components/stocks/Stocks.vue
@@ -8,16 +8,6 @@
     import Stock from './Stock.vue';
 
     export default {
-        data() {
-            return {
-                stocks: [
-                    {id: 1, name: 'BMW', price: 110},
-                    {id: 2, name: 'Apple', price: 200},
-                    {id: 3, name: 'Google', price: 250},
-                    {id: 4, name: 'Twitter', price: 8}
-                ]
-            }
-        },
         components: {
             appStock: Stock
         }
